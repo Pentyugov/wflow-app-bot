54d2191706415593a46e69bc4f0b05959c9363ea
apentyugov <pentyugov@icloud.com>
2022-07-31 19:04:52 +0400

    Initial commit

--- src/main/java/com/pentyugov/wflowappbot/WflowAppBotApplication.java
+++ src/main/java/com/pentyugov/wflowappbot/WflowAppBotApplication.java
@@ -2,12 +2,13 @@ package com.pentyugov.wflowappbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, DataSourceAutoConfiguration.class})
 public class WflowAppBotApplication {
 
 
